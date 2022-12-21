db927fd1e1eb30d3391729796b75716c92089fad
Fabio Bove <fabio.bove.dr@gmail.com>
2022-12-21 17:09:39 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,3 +3,10 @@ from pathlib import Path
 sys.path[0] = str(Path(sys.path[0]).parent)
 
 from src.ImageWithWordBuilder import ImageWithWordBuilder
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
