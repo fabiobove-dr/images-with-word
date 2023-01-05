0c3ad89c735c24dc7751a9626bc08819ae580573
Fabio Bove <fabio.bove.dr@gmail.com>
2023-01-05 09:21:33 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,7 +5,6 @@ from pathlib import Path
 sys.path[0] = str(Path(sys.path[0]).parent)
 
 from src.ImageWithWord import ImageWithWord
-from src.InstaConnect import InstagramAPI
 
 # Load the oauth_settings.yml file
 stream          = open('../oauth.yml', 'r')
@@ -22,10 +21,6 @@ def main():
     # save the image to a file
     file_name = time.time()
     image_with_word.save_image(f'../images/{file_name}.png')
-
-    instagram_api = InstagramAPI(app_id, app_secret)
-    instagram_api.exchange_token(token_client)
-    instagram_api.post_image("../images/test.png", "This is my image!", ["#cool", "#fun"])
     
 if __name__ == "__main__":
     main()
